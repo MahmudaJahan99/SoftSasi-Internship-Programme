b59a372e1ac1b5e43729c4f609c509358ab21306
DepressedPotatok <mj.mjahan045@gmail.com>
2026-08-24 22:22:06 +0600

    express scaffold and backend frontend connect

--- monorepo-learning/apps/web/src/App.tsx
+++ monorepo-learning/apps/web/src/App.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import axios from "axios";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/health")
+      .then((response) => {
+        console.log(response.data);
+        setMessage(response.data.message);
+      })
+      .catch((error) => {
+        console.error("API error:", error);
+      });
+  }, []);
+
   return (
     <>
       <Home />
+      <h1>{message}</h1>
     </>
   );
 }
